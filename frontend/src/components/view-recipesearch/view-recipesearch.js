92982cd2338ee268e59420a41a1f6c814f69d17b
Andreea <andreean95@yahoo.com>
2017-06-15 16:57:38 +0200

    Autocomplete tag search

--- frontend/src/components/view-recipesearch/view-recipesearch.js
+++ frontend/src/components/view-recipesearch/view-recipesearch.js
@@ -4,6 +4,49 @@ import angular from 'angular';
 
 import ViewRecipesearchComponent from './view-recipesearch.component';
 
+var app = angular.module('ViewRecipesearch', ['ngTagsInput']);
+app.controller('ViewRecipesearchComponentController', function($scope, tags) {
 
-export default angular.module('ViewRecipesearch', [])
+$scope.loadTags = function(query) {
+    return tags.load();
+};
+});
+
+app.service('tags', function($q) {
+    var tags = [
+        { "text": "Tag1" },
+        { "text": "Tag2" },
+        { "text": "Tag3" },
+        { "text": "Tag4" },
+        { "text": "Tag5" },
+        { "text": "Tag6" },
+        { "text": "Tag7" },
+        { "text": "Tag8" },
+        { "text": "Tag9" },
+        { "text": "Tag10" }
+    ];
+
+    this.load = function() {
+        var deferred = $q.defer();
+        deferred.resolve(tags);
+        return deferred.promise;
+    };
+
+
+
+
+    /*   export default angular.module('ViewRecipesearch', ['ngTagsInput'])
     .component(ViewRecipesearchComponent.name, new ViewRecipesearchComponent);
+   .controller('ViewRecipesearchComponentController', function($scope, $http) {
+        $scope.tags = [
+            { text: 'just' },
+            { text: 'some' },
+            { text: 'cool' },
+            { text: 'tags' }
+        ];
+        $scope.loadTags = function(query) {
+            return $http.get('/tags?query=' + query);
+        };*/
+    });
+//export default angular.module('ViewRecipesearch', [])
+ //   .component(ViewRecipesearchComponent.name, new ViewRecipesearchComponent);
