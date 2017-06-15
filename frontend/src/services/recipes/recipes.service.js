553b00408dfd82c282bcc9ff359c10a0365668e8
Andreea <andreean95@yahoo.com>
2017-06-15 13:23:30 +0200

    RecipesServices

---
+++ frontend/src/services/recipes/recipes.service.js
@@ -0,0 +1,81 @@
+'use strict';
+
+
+export default class RecipesService {
+
+    static get $inject(){
+        return ['$http', 'API_URL'];
+    }
+
+    constructor($http,API_URL) {
+        this.$http = $http;
+        this.resourceUrl = `${ API_URL }/recipes/`;
+
+    }
+
+    static get name(){
+        return 'recipesService';
+    }
+
+    list() {
+
+        let url = this.resourceUrl;
+        return this.$http.get(url).then(responce => {
+
+            return new Promise((resolve, reject) => {
+                resolve(responce.data);
+
+            });
+
+        });
+
+    }
+
+    get(id) {
+        let url = `${ this.resourceUrl }${ id }`;
+        return this.$http.get(url).then(responce => {
+
+            return new Promise((resolve, reject) => {
+                resolve(responce.data);
+            });
+
+        })
+    }
+
+
+    create(recipe) {
+        let url = this.resourceUrl;
+        return this.$http.post(url,recipe).then(responce => {
+
+            return new Promise((resolve, reject) => {
+                resolve(responce.data);
+            });
+
+        })
+    }
+
+    delete(id) {
+        let url = `${ this.resourceUrl }${ id }`;
+        return this.$http.delete(url).then(responce => {
+
+            return new Promise((resolve, reject) => {
+                resolve(responce.status);
+            });
+
+        })
+    }
+
+    update(recipe) {
+
+        let url = `${ this.resourceUrl }${ recipe['_id'] }`;
+        return this.$http.put(url,recipe).then(responce => {
+
+            return new Promise((resolve, reject) => {
+                resolve(responce.data);
+            });
+
+        })
+    }
+
+
+}
