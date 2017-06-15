cfe1864ae9548213e26b69a22f4054dce81aa74e
Hamza Zafar <hamza.zafar1993@gmail.com>
2017-06-15 12:42:25 +0200

    adding routes

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -17,6 +17,11 @@ import AppContent from './components/app-content/app-content';
 import ViewLogin from './components/view-login/view-login';
 import ViewRegister from './components/view-register/view-register';
 
+import ViewRecipes from './components/view-recipes/view-recipes';
+import ViewRecipe from './components/view-recipe/view-recipe';
+import ViewRecipeEdit from './components/view-recipe-edit/view-recipe-edit';
+import ViewRecipeCreate from './components/view-recipe-create/view-recipe-create';
+
 let app = angular.module('app', [
     uiRouter,
     angularMaterial,
@@ -24,7 +29,11 @@ let app = angular.module('app', [
     UserService.name,
     AppContent.name,
     ViewLogin.name,
-    ViewRegister.name
+    ViewRegister.name,
+    ViewRecipes.name,
+    ViewRecipe.name,
+    ViewRecipeEdit.name,
+    ViewRecipeCreate.name,
 ]);
 
 app.constant('API_URL', 'http://localhost:3000/api');
