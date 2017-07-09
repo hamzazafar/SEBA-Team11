8acb42fbf79bd77179d910e10b12929640171784
Teodora Lata <teodora.lata@yahoo.com>
2017-07-09 13:02:35 +0200

    - remove not necessary methods

--- frontend/src/components/view-group/view-group.component.js
+++ frontend/src/components/view-group/view-group.component.js
@@ -23,19 +23,10 @@ class ViewGroupComponentController{
     constructor($state,GroupsService,UserService){
         this.$state = $state;
         this.group = {};
-        this.model = {};
         this.GroupsService = GroupsService;
         this.UserService = UserService;
-        this.review_comment = "";
-        this.group.reviews = [];
-        this.stars = 0;
-
-
     }
-    $onInit() {
-        //Clone the Group Data
-        this.model = JSON.parse(JSON.stringify(this.group))
-    }
+
 
     save() {
         let _id = this.group['_id'];
@@ -70,56 +61,6 @@ class ViewGroupComponentController{
             this.$state.go('login',{});
         }
     };
-
-    addReview(){
-      var review = {};
-      review.author = this.UserService.getCurrentUser().username;
-      review.comment = this.review_comment;
-      review.published_date = new Date();
-      review.stars = this.stars;
-      this.model.reviews.push(review);
-      this.review_comment = "";
-
-
-      $('#myModal').modal('hide');
-
-      this.save();
-
-    };
-
-    deleteReview(i){
-        var index = this.model.reviews.indexOf(i);
-        if (index > -1) {
-            this.model.reviews.splice(index, 1);
-        }
-        this.save();
-    }
-
-    getRating() {
-      var countReviews = this.model.reviews.length;
-      console.log(countReviews)
-
-      var sumReviews = 0;
-      var i = 0;
-      for ( i = 0; i < this.model.reviews.length; i++) {
-        sumReviews += +this.model.reviews[i].stars;
-        console.log(this.model.reviews[i].stars)
-      }
-      console.log(sumReviews)
-      if (countReviews == 0) {
-        return 5;
-      } else {
-
-       return Math.round(sumReviews/countReviews);
-     }
-
-    }
-
-    getNumberofRevies(){
-        var countReview = this.model.reviews.length;
-        return countReview;
-    }
-
 
     loadImage(image) {
     return require('file-loader!../../assets/' + image);
